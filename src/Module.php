88b5691729600837b70dd9e980f2ac807d9ae519
borisey <51847223+borisey@users.noreply.github.com>
2025-12-27 17:01:48 +0300

    fix

---
+++ src/Module.php
@@ -0,0 +1,9 @@
+<?php
+namespace Borisey\Yii2DesignPatterns;
+
+use yii\base\Module as BaseModule;
+
+class Module extends BaseModule
+{
+    public $controllerNamespace = 'Borisey\Yii2DesignPatterns\controllers';
+}
